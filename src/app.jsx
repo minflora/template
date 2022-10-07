e89bbf84f92af31f7b1ab4ace179e228dcbfcdff
minflora <wleka0402@gmail.com>
2022-10-07 14:05:24 +0900

    template finish

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,8 @@
-import React from 'react';
-import './app.css';
+import React from "react";
+import "./app.css";
 
 function App() {
-  return 
-    <h1>Hello :)</h1>;
+  return <h1>Hello :)</h1>;
 }
 
 export default App;
